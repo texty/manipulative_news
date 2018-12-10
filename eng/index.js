1847f0f0db20d3069c3daa25ccee425c1084d5f8
Nadiia Romanenko <nadiia.romanenko@gmail.com>
2018-12-10 14:21:21 +0200

    fix damn canvas

--- eng/index.js
+++ eng/index.js
@@ -86,10 +86,6 @@ var fullscreen_fig = function (scroller=null) {
             return window.innerHeight - $(this).closest('section').find('.h3').get(0).offsetHeight;
         });
 
-    $('#topic_viz canvas')
-        .attr('width', function () { return window.innerWidth })
-        .attr('height', function () { return `${window.outerHeight - $(this).closest('section').find('.h3').get(0).offsetHeight}` });
-
     $('#wire_vru #spread_wire div.hline_day_feed, #spread_wire div#tline_text, #sites')
         .css( 'min-height', window.innerHeight );
 
