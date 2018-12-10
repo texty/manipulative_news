0ade3301030b6b0383987be815e24ca2185a8dcb
Nadiia Romanenko <nadiia.romanenko@gmail.com>
2018-12-10 12:54:01 +0200

    canvas fix

--- eng/index.js
+++ eng/index.js
@@ -88,7 +88,7 @@ var fullscreen_fig = function (scroller=null) {
 
     $('#topic_viz canvas')
         .attr('width', function () { window.innerWidth })
-        .attr('height', function () { return window.outerHeight });
+        .attr('height', function () { return `${window.outerHeight - $(this).closest('section').find('.h3').get(0).offsetHeight}` });
 
     $('#wire_vru #spread_wire div.hline_day_feed, #spread_wire div#tline_text, #sites')
         .css( 'min-height', window.innerHeight );
