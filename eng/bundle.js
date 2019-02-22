a35f047ae9d8722912328bd9ddcd94e417fc7057
Nadiia Romanenko <nadiia.romanenko@gmail.com>
2019-02-22 21:44:33 +0200

    packege.json and fix that damn safari z-index

--- eng/bundle.js
+++ eng/bundle.js
@@ -1,5 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-// sass style.scss style.css && browserify index.js -o bundle.js && postcss style.css -o style_prefixed.css --use autoprefixer
+// sass style.scss style.css && browserify index.js -o bundle.js && postcss style.css -o style_prefixed.css --use autoprefixer && browserify eng/index.js -o eng/bundle.js
 // sass for win only - if no normal watcher
 const $ = require("jquery");
 require('intersection-observer');
